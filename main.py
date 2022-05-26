38c89e261b427104dd8bcad95eba81ac8ea0b1e6
Logan Mack <loganarmack@gmail.com>
2022-05-26 14:04:05 -0400

    Fully working script

---
+++ main.py
@@ -0,0 +1,48 @@
+from ytmusicapi import YTMusic
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import json
+import re
+import sys
+
+load_dotenv()
+
+name = "Vicky's Chill Playlist"
+desc = "Some chil beats curated by Vli"
+url = "https://open.spotify.com/playlist/56wCm5iHTas4p7CwoN0T4e?si=30591c0e92004328"
+playlist_id = re.findall(r"playlist\/([a-zA-Z0-9]*)", url)[0]
+
+if not playlist_id:
+    print("Invalid playlist url.")
+    sys.exit()
+
+ytmusic = YTMusic('headers_auth.json')
+yt_playlist_id = ytmusic.create_playlist("Vicky's Chill Plalist", "Some chill beats curated by Vli")
+
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+playlist = sp.playlist(playlist_id)
+
+for item in playlist["tracks"]["items"]:
+    song = item["track"]
+    artists = ""
+    for artist in song["artists"]:
+        artists += artist["name"] + " "
+    name = song["name"]
+    
+    id = None
+    search_results = ytmusic.search(artists + name)
+    for result in search_results:
+        if "song" in result["resultType"]:
+            id = result["videoId"]
+            break
+
+    if id:
+        ytmusic.add_playlist_items(yt_playlist_id, [id])
+        print(f"Adding song {name} to the playlist.")
+    
+    else:
+        print(f"Unable to find song {name}")
+
+print("All done!")
